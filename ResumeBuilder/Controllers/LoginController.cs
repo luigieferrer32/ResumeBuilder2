95c8371e20e77df76367ceaecc5d6bcbc86d926f
arron sealmoy <arronsealmoy@gmail.com>
2018-06-09 20:50:26 +0800

    8:50

--- ResumeBuilder/Controllers/LoginController.cs
+++ ResumeBuilder/Controllers/LoginController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using BusinessLogic.Manager;
+using DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace ResumeBuilder.Controllers
 {
@@ -15,13 +18,13 @@ namespace ResumeBuilder.Controllers
             return View();
         }
 
-        //public JsonResult AuthenticateUser(USER user)
-        //{
-        //    UserManager userManager = new UserManager();
-        //    var access = userManager.AuthenticateUser(user);
+        public JsonResult AuthenticateUser(USER user)
+        {
+            UserManager userManager = new UserManager();
+            var access = userManager.AuthenticateUser(user);
 
-        //    return Json(new { access = access }, JsonRequestBehavior.AllowGet);
-        //}
+            return Json(new { access = access }, JsonRequestBehavior.AllowGet);
+        }
 
     }
 }
