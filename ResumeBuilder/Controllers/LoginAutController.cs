92b3ea00ebe44e123a60179d689217cb634f307b
arron sealmoy <arronsealmoy@gmail.com>
2018-06-21 17:09:19 +0800

    Web Api google

---
+++ ResumeBuilder/Controllers/LoginAutController.cs
@@ -0,0 +1,19 @@
+﻿using ResumeBuilder.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ResumeBuilder.Controllers
+{
+    public class LoginAutController : ApiController
+    {
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult ExternalLogin()
+        {
+            return new ChallengeResult("Google", "/api/home", this.Request);
+        }
+    }
+}
---
+++ ResumeBuilder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+using ResumeBuilder.App_Start;
+
+namespace ResumeBuilder
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            SecurityConfig.Configure(app);
+            WebApiConfig.Configure(app);
+        }
+    }
+}
